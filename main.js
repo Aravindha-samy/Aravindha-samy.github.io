9713499608e877967dd43691fdfe300f5f8147f3
Aravindha-samy <aravindhasamychs2017@gmail.com>
2023-11-18 11:29:12 +0000

    created index.html,styulesheet.css and main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-satisfies 
+var typed = new Typed(".text", {
+    strings: ["Full Stack Developer"," Web Developer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true,
+  })
+
+  
